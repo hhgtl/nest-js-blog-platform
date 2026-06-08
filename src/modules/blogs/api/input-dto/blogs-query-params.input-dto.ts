2117bc7b6f04d55e620b3d17c1169016489e772a
petro <petrosahal66@gmail.com>
2026-06-08 19:33:26 +0300

    check

--- src/modules/blogs/api/input-dto/blogs-query-params.input-dto.ts
+++ src/modules/blogs/api/input-dto/blogs-query-params.input-dto.ts
@@ -1,11 +1,12 @@
 import { BlogsSortBy } from './blogs-sort-by';
-import { IsEnum, IsString } from 'class-validator';
+import { IsEnum, IsOptional, IsString } from 'class-validator';
 import { BaseQueryParams } from '../../../../core/dto/base.query-params.input-dto';
 
 export class GetBlogsQueryParams extends BaseQueryParams {
   @IsEnum(BlogsSortBy)
   sortBy = BlogsSortBy.CreatedAt;
 
+  @IsOptional()
   @IsString()
-  searchNameTerm: string;
+  searchNameTerm?: string;
 }
