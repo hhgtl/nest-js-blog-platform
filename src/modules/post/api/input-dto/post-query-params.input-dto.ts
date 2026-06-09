d46fa15ebcddd34ad280421ad287417a2724afea
petro <petrosahal66@gmail.com>
2026-06-09 23:43:29 +0300

    Revert "check"
    This reverts commit 84c0796d7e1adc9545e7aedbf36d2873c5394a5b.

--- src/modules/post/api/input-dto/post-query-params.input-dto.ts
+++ src/modules/post/api/input-dto/post-query-params.input-dto.ts
@@ -1,6 +1,5 @@
 import { BaseQueryParams } from '../../../../core/dto/base.query-params.input-dto';
 import { IsEnum, IsOptional } from 'class-validator';
-import { Transform, TransformFnParams } from 'class-transformer';
 
 export enum PostSortBy {
   CreatedAt = 'createdAt',
@@ -8,11 +7,6 @@ export enum PostSortBy {
 
 export class GetPostsQueryParams extends BaseQueryParams {
   @IsOptional()
-  @Transform((params: TransformFnParams): PostSortBy => {
-    const value: unknown = params.value;
-    if (value === undefined || value === '') return PostSortBy.CreatedAt;
-    return value as PostSortBy;
-  })
   @IsEnum(PostSortBy)
   sortBy = PostSortBy.CreatedAt;
 }
