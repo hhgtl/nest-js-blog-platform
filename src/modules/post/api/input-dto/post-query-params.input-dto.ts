510641a8132e8f3c31d9d2b87a54892fab1bed3b
petro <petrosahal66@gmail.com>
2026-06-09 17:47:55 +0300

    check

--- src/modules/post/api/input-dto/post-query-params.input-dto.ts
+++ src/modules/post/api/input-dto/post-query-params.input-dto.ts
@@ -1,11 +1,12 @@
 import { BaseQueryParams } from '../../../../core/dto/base.query-params.input-dto';
-import { IsEnum } from 'class-validator';
+import { IsEnum, IsOptional } from 'class-validator';
 
 export enum PostSortBy {
   CreatedAt = 'createdAt',
 }
 
 export class GetPostsQueryParams extends BaseQueryParams {
+  @IsOptional()
   @IsEnum(PostSortBy)
   sortBy = PostSortBy.CreatedAt;
 }
