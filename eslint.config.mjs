f365146f707913729c508c1472b506781a4b9173
petro <petrosahal66@gmail.com>
2026-09-15 23:18:13 +0300

    check

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -30,6 +30,7 @@ export default tseslint.config(
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn',
       '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-non-null-asserted-optional-chain': 'warn',
       "prettier/prettier": ["error", { endOfLine: "auto" }],
     },
   },
